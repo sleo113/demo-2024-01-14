7a1ee1973e3f78234d32323d5c18d4ad056f7ec0
JeUk Park <qkrwpdnr4531@naver.com>
2024-01-14 20:17:31 +0900

    세팅

---
+++ src/main/java/com/ll/demo20240114/Demo20240114Application.java
@@ -0,0 +1,13 @@
+package com.ll.demo20240114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20240114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo20240114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/demo20240114/Demo20240114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.demo20240114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20240114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
